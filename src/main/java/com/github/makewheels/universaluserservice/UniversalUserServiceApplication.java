328461c1915a9fcbb320d8a59dbd78cac383c3dc
makewheels <makewheels>
2021-08-20 21:18:00 +0800

    Initial commit

---
+++ src/main/java/com/github/makewheels/universaluserservice/UniversalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.universaluserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversalUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/universaluserservice/UniversalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.universaluserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
