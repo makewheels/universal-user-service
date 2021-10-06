d512e0aeac89220605afb887adaf35dd4f22e7e1
miuser <miuser@email.com>
2021-10-06 16:28:13 +0800

    1

--- src/main/java/com/github/makewheels/universaluserservice/UniversalUserServiceApplication.java
+++ src/main/java/com/github/makewheels/universaluserservice/UniversalUserServiceApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import javax.annotation.Resource;
-
-@EnableDiscoveryClient
+`@EnableDiscoveryClient
 @SpringBootApplication
 public class UniversalUserServiceApplication {
 
